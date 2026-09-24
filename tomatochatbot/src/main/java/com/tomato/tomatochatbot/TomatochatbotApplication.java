6ce07d4d6a9ddd34a60f73e4daed29d15725cbc9
Anjali28-ish <anjalikumarimishra28@gmail.com>
2026-09-24 14:00:34 +0530

    Initial commit

---
+++ tomatochatbot/src/main/java/com/tomato/tomatochatbot/TomatochatbotApplication.java
@@ -0,0 +1,13 @@
+package com.tomato.tomatochatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomatochatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomatochatbotApplication.class, args);
+	}
+
+}
---
+++ tomatochatbot/src/test/java/com/tomato/tomatochatbot/TomatochatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomato.tomatochatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomatochatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
